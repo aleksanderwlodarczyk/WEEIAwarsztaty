8e9e9c5cb24876161242ba5798adad1ef42bdd71
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-02-08 02:37:17 +0100

    Początek projektu
    - Zgrane darmowe assety graficzne
    - Skrypt na poruszanie postacią
    - Skrypt na podążanie kamery za obiektem

---
+++ Assets/Scripts/CameraFollowingObject.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollowingObject : MonoBehaviour {
+
+	public GameObject objectToFollow;
+
+	private Vector3 offset;
+	private bool changingBoth;
+
+	void Start () {
+		changingBoth = false;
+	}
+	
+
+	void Update () {
+		offset = objectToFollow.transform.position - transform.position;
+		Debug.Log (offset);
+
+		if (offset.x > 5f && offset.y > 2f) {
+			Vector3 offsetWhenChange = new Vector3 (5f, 2f, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+			changingBoth = true;
+		}
+
+		if (offset.x > 5f && offset.y < -2f) {
+			Vector3 offsetWhenChange = new Vector3 (5f, -2f, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+			changingBoth = true;
+		}
+
+		if (offset.x < -5f && offset.y > 2f) {
+			Vector3 offsetWhenChange = new Vector3 (-5f, 2f, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+			changingBoth = true;
+		}
+
+		if (offset.x < -5f && offset.y < -2f) {
+			Vector3 offsetWhenChange = new Vector3 (-5f, -2f, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+			changingBoth = true;
+		}
+
+
+
+
+		if (offset.x > 5f && !changingBoth) {
+			Vector3 offsetWhenChange = new Vector3 (5f, offset.y, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+		}
+
+		if (offset.x < -5f && !changingBoth) {
+			Vector3 offsetWhenChange = new Vector3 (-5f, offset.y, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+		}
+
+		if (offset.y > 2f && !changingBoth) {
+			Vector3 offsetWhenChange = new Vector3 (offset.x, 2f, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+		}
+
+		if (offset.y < -2f && !changingBoth) {
+			Vector3 offsetWhenChange = new Vector3 (offset.x, -2f, offset.z);
+			transform.position = objectToFollow.transform.position - offsetWhenChange;
+		}
+
+		changingBoth = false;
+	}
+		
+}
---
+++ Assets/Scripts/KeyboardMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class KeyboardMovement : MonoBehaviour {
+
+	public float speed = 10f;
+	public float jumpForce = 10f;
+	public float maxSpeed = 7f;
+
+	private bool canJump;
+	private Rigidbody2D rb2d;
+	private float horizontal;
+	private float realSpeed;
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		realSpeed = speed;
+		canJump = true;
+	}
+	
+
+	void Update () {
+
+		horizontal = Input.GetAxis ("Horizontal");
+
+		rb2d.AddForce (Vector2.right * horizontal * realSpeed);
+
+		if ((rb2d.velocity.x * horizontal) < 0)
+			realSpeed = 10 * speed;
+		else
+			realSpeed = speed;
+
+		if ((Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow)) && canJump) {
+			rb2d.AddForce (Vector2.up * jumpForce * 3);
+			canJump = false;
+		}
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
+			realSpeed = 0;
+		else
+			realSpeed = speed;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Ground")
+			canJump = true;
+	}
+}
