006708da77a2945c040df9f848b92f314973882e
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-02-09 17:16:55 +0100

    meta
    skrypt na wyswietlanie winscreena, bez zatrzymania postaci
    
    prefab skrzyni(mety)

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour {
+
+	private GameObject winningScreen;
+
+	void Start () {
+		winningScreen = GameObject.Find ("winscreen");
+		winningScreen.SetActive (false);
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			winningScreen.SetActive (true);
+		}
+	}
+
+}
