b89d04aa11d12941fccb46b9637b01e01415855c
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-02-13 21:30:33 +0100

    enemy(bugged)

---
+++ Assets/Scripts/AIGroundCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIGroundCheck : MonoBehaviour {
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            gameObject.transform.parent.gameObject.GetComponent<EnemyMoving>().canGo = false;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            gameObject.transform.parent.gameObject.GetComponent<EnemyMoving>().canGo = true;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyMoving.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoving : MonoBehaviour {
+
+    public bool canGo;
+    public float speed = 10f;
+
+    private GameOver gOver;
+    private Rigidbody2D rb2d;
+
+	void Start () {
+        canGo = true;
+        gOver = GameObject.Find("GameOver").GetComponent<GameOver>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void FixedUpdate()
+    {
+        if(canGo && gOver.playing)
+        {
+            rb2d.AddForce(Vector2.left * speed * Mathf.Sin(Time.time*3));
+        }
+    }
+	
+
+}
