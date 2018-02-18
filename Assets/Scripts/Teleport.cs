40777774df9185d2af1aa40c118acd25fcde91d8
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-02-18 03:36:02 +0100

    teleporty

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+	public Transform outputTransform;
+	public string typeToTeleport;
+
+	private Transform objectToTeleport;
+	
+	void Teleporting(){
+		objectToTeleport.position = outputTransform.position;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == typeToTeleport) {
+			objectToTeleport = other.gameObject.transform;
+			Teleporting ();
+		}
+	}
+}
