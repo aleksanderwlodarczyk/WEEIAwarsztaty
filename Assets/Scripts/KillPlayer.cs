3695ac60dc67e07efce3de3738fcd5a719c59b85
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-03-16 00:43:24 +0100

    death particles

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -6,12 +6,15 @@ public class KillPlayer : MonoBehaviour {
 
 	private GameOver gOver;
 
+	public GameObject deathParticles;
+
 	void Start(){
 		gOver = GameObject.Find ("GameOver").GetComponent<GameOver> ();
 	}
 
 	void OnCollisionEnter2D(Collision2D coll){
 		if (coll.gameObject.tag == "Player") {
+			Instantiate (deathParticles, gameObject.transform.position, gameObject.transform.rotation);
 			coll.gameObject.GetComponent<Killable> ().Kill ();
 			gOver.Defeat ();
 		}
